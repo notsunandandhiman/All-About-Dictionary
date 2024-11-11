cb88fdb0a2129d7810dab9a0ae2a26db58587eba
notsunandandhiman <150802724+notsunandandhiman@users.noreply.github.com>
2024-11-11 21:25:59 +0530

    Add files via upload

---
+++ Country Code.py
@@ -0,0 +1,11 @@
+country_code = {'India': '0091',
+                 'Australia': '0061',
+                 'USA': '001',}
+
+print("Country code for India -")
+print(country_code.get('India','Not Found'))
+      
+print("Country code for Japan -")
+print(country_code.get('Japan','Not Found'))
+
+
